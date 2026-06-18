c792bb8ed53ff6738b9f6d64ad4eea496493617c
shakehor-coder <shakehor@gmail.com>
2026-06-18 12:14:51 +0300

    Initial commit: Project Task Manager application with SQLite support and MVVM architecture.

---
+++ MauiProgram.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Logging;
+using ProjectManager.Services;
+using ProjectManager.ViewModels;
+using ProjectManager.Views;
+
+namespace ProjectManager;
+
+/// <summary>
+/// Класс конфигурации приложения, отвечающий за инициализацию DI-контейнера и сервисов.
+/// </summary>
+public static class MauiProgram
+{
+    /// <summary>
+    /// Создает и настраивает экземпляр приложения.
+    /// </summary>
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        // Регистрация сервисов согласно требованиям: DatabaseService как Singleton, страницы как Transient.
+        builder.Services.AddSingleton<DatabaseService>();
+        builder.Services.AddTransient<MainViewModel>();
+        builder.Services.AddTransient<MainPage>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
