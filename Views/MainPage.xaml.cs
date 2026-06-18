a36333f9f5eb61f2374206174d7da05fc1cbeb65
shakehor-coder <shakehor@gmail.com>
2026-06-18 12:14:48 +0300

    Initial commit: Project Task Manager application with SQLite support and MVVM architecture.

---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using ProjectManager.ViewModels;
+
+namespace ProjectManager.Views;
+
+/// <summary>
+/// Главная страница приложения для отображения и управления списком задач.
+/// </summary>
+public partial class MainPage : ContentPage
+{
+    /// <summary>
+    /// Инициализирует новый экземпляр класса MainPage.
+    /// </summary>
+    /// <param name="viewModel">Экземпляр ViewModel, внедряемый через DI-контейнер.</param>
+    public MainPage(MainViewModel viewModel)
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+}
